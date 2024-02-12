1e303597cccb8c28f6ba66f92403eeb9c05906af
Cipher <petergubanov@gmail.com>
2024-02-12 17:18:51 -0500

    test

--- ContactManager.java
+++ ContactManager.java
@@ -2,30 +2,30 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-// Define the ContactManager class
-class ContactManager {
-    // List to store Contact objects
+public class ContactManager {
     private List<Contact> contacts;
 
-    // Constructor to initialize the ContactManager
     public ContactManager() {
         contacts = new ArrayList<>();
     }
 
-    // Method to add a new Contact
     public void addContact(Contact contact) {
-        // Your code here
+        contacts.add(contact);
     }
 
-    // Method to remove a Contact by name
     public void removeContact(String name) {
-        // Your code here
+        Iterator<Contact> iterator = contacts.iterator();
+        while (iterator.hasNext()) {
+            Contact contact = iterator.next();
+            if (contact.getName().equals(name)) {
+                iterator.remove();
+            }
+        }
     }
 
-    // Method to list all Contacts
     public void listContacts() {
-        // Your code here
+        for (Contact contact : contacts) {
+            System.out.println(contact);
+        }
     }
-
-
 }
