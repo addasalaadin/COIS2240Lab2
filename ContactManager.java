35e254ca44c96bbfdf8672b6c187f36f25ce22d6
Nuha <nuhaabdulgafoor@trentu.ca>
2024-02-12 17:18:40 -0500

    commiting

--- ContactManager.java
+++ ContactManager.java
@@ -8,24 +8,39 @@ class ContactManager {
     private List<Contact> contacts;
 
     // Constructor to initialize the ContactManager
-    public ContactManager() {
+    public ContactManager()
+     {
         contacts = new ArrayList<>();
     }
 
     // Method to add a new Contact
-    public void addContact(Contact contact) {
-        // Your code here
+    public void addContact(Contact contact) 
+    {
+        contacts.add(contact);
     }
 
-    // Method to remove a Contact by name
-    public void removeContact(String name) {
-        // Your code here
+   // Method to remove a Contact by name
+   public void removeContact(String name) {
+    Iterator<Contact> iterator = contacts.iterator();
+    while (iterator.hasNext()) {
+        Contact contact = iterator.next();
+        if (contact.getName().equals(name)) {
+            iterator.remove(); // Remove the contact from the list
+        }
+    }
+}
+
+// Method to list all Contacts
+public void listContacts() {
+    if (contacts.isEmpty()) {
+        System.out.println("No contacts available.");
+    } else {
+        System.out.println("List of contacts:");
+        for (Contact contact : contacts) {
+            System.out.println("Name: " + contact.getName() + ", Phone Number: " + contact.getPhoneNumber());
+        }
     }
 
-    // Method to list all Contacts
-    public void listContacts() {
-        // Your code here
-    }
 
-
+}
 }
--- Program.java
+++ Program.java
@@ -1,3 +1,4 @@
+
 public class Program {
         // Main method for testinbg
         public static void main(String[] args) {
