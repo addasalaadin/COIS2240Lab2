05b705d9a41ddb02fff83810731c0ea3f799f2bf
MarkMuazami <157428972+MarkMuazami@users.noreply.github.com>
2024-02-12 17:13:28 -0500

    Second Commit

--- Program.java
+++ Program.java
@@ -9,12 +9,14 @@ public class Program {
             manager.addContact(new Contact("Bob", "bob@example.com", "098-765-4321"));
     
             // List contacts
+            System.out.println("\nList of Contacts: ");
             manager.listContacts();
     
             // Remove a contact
-            manager.removeContact("Alaadin");
-    
+            manager.removeContact("john");
+
             // List contacts again to see the change
+            System.out.println("\nList of Contacts: ");
             manager.listContacts();
         }
 }
