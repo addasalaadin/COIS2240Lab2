15a03609c755db81101bd3cc353572a3d8ddefb4
Duha <duhakhan@trentu.ca>
2024-02-12 17:17:30 -0500

    Committing

--- Contact.java
+++ Contact.java
@@ -1,16 +1,14 @@
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 // Define the Contact class
-class Contact {
+class Contact
+{
     // Properties of the Contact class
     private String name;
     private String email;
     private String phoneNumber;
 
     // Constructor to initialize a new Contact object
-    public Contact(String name, String email, String phoneNumber) {
+    public Contact(String name, String email, String phoneNumber)
+    {
         this.name = name;
         this.email = email;
         this.phoneNumber = phoneNumber;
@@ -18,7 +16,8 @@ class Contact {
 
     // Implement the toString method for Contact
     @Override
-    public String toString() {
+    public String toString()
+    {
         return "Contact{" +
                 "name='" + name + '\'' +
                 ", email='" + email + '\'' +
@@ -27,5 +26,34 @@ class Contact {
     }
 
     // Getters and Setters (if necessary)
-    // ...
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setPhoneNumber(String phoneNumber)
+    {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhoneNumber()
+    {
+        return phoneNumber;
+    }
+    
 }
