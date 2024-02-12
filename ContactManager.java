6c7fd41d25ae62eb166375ebb6b09523d2414dca
dongjaesuh <157067730+dongjaesuh@users.noreply.github.com>
2024-02-12 17:09:00 -0500

    Update ContactManager.java
    completed code

--- ContactManager.java
+++ ContactManager.java
@@ -14,18 +14,36 @@ class ContactManager {
 
     // Method to add a new Contact
     public void addContact(Contact contact) {
-        // Your code here
+        contacts.add(contact);
     }
 
     // Method to remove a Contact by name
     public void removeContact(String name) {
-        // Your code here
+        // setup a contact that needs to be removed(currently null)
+        Contact contactToRemove = null;
+        
+        // search for the contact through the array list, and break the loop if found
+        for (Contact contact : contacts) {
+            if (contact.getName().equals(name)) {
+                contactToRemove = contact;
+                break;
+            }
+        }
+
+        // remove the contact if found, if not return a message to indicate that it wasn't found
+        if (contactToRemove != null) {
+            contacts.remove(contactToRemove);
+            System.out.println("Contact " + name + " was removed successfully!");
+        } else {
+            System.out.println("Contact " + name + " was not found.");
+        }
     }
 
     // Method to list all Contacts
     public void listContacts() {
-        // Your code here
+        // print out the contacts in order to list them off to the user
+        for (Contact contact : contacts) {
+            System.out.println(contact);
+        }
     }
-
-
 }
