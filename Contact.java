69771ccb09f11b41db37b90436cf9cbc8853b130
Darren Chen <darrenchen@trentu.ca>
2024-02-12 23:19:33 -0500

    Completed Lab 2

--- Contact.java
+++ Contact.java
@@ -1,7 +1,3 @@
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 // Define the Contact class
 class Contact {
     // Properties of the Contact class
@@ -28,4 +24,13 @@ class Contact {
 
     // Getters and Setters (if necessary)
     // ...
+    // Getter method to retrieve the value of the 'name' property
+    public String getName() {
+        return name;
+    }
+
+    // Setter method to set the value of the 'name' property
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- ContactManager.java
+++ ContactManager.java
@@ -15,17 +15,24 @@ class ContactManager {
     // Method to add a new Contact
     public void addContact(Contact contact) {
         // Your code here
+        contacts.add(contact);
     }
 
     // Method to remove a Contact by name
     public void removeContact(String name) {
-        // Your code here
+        Iterator<Contact> iterator = contacts.iterator();
+        while (iterator.hasNext()) {
+            Contact contact = iterator.next();
+            if (contact.getName().equals(name)) {
+                iterator.remove();
+            }
+        }
     }
 
     // Method to list all Contacts
     public void listContacts() {
-        // Your code here
+        for (Contact contact : contacts) {
+            System.out.println(contact);
+        }
     }
-
-
 }
