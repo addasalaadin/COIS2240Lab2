d7f6100ccb2d8194709dff3cd1790609ed5641e5
mariammerza <mariammerza@trentu.ca>
2024-02-12 19:39:39 -0500

    changes

--- ContactManager.java
+++ ContactManager.java
@@ -15,17 +15,36 @@ class ContactManager {
     // Method to add a new Contact
     public void addContact(Contact contact) {
         // Your code here
+        contacts.add(0, contact);
+        contacts.add(1, contact);
+        contacts.add(2, contact);
+        System.out.println(contact + "has been added successfully");
     }
 
     // Method to remove a Contact by name
-    public void removeContact(String name) {
+    public Boolean removeContact(String name) {
         // Your code here
+        Iterator<Contact> iterator = contacts.iterator();
+        while (iterator.hasNext()) {
+            Contact contact = iterator.next();
+            if (contact.getName().equalsIgnoreCase(name)) {
+                iterator.remove();
+                System.out.println("The contact has been successfully removed");
+                break;
+            } else {
+                System.out.println("Contact not found.");
+                break;
+            }
+        }
+        return true;
     }
 
     // Method to list all Contacts
     public void listContacts() {
         // Your code here
+        for (Contact contact : contacts) {
+            System.out.println(contact);
+        }
+
     }
-
-
 }
