bbe6583d6510b6a9eb06e4a1350c754debc69c0a
devpatel4 <devpatel4@trentu.ca>
2024-02-16 22:11:25 -0500

    Configure 3 Methods(addContact, removeContact and listContacts)

--- ContactManager.java
+++ ContactManager.java
@@ -8,24 +8,37 @@ class ContactManager {
     private List<Contact> contacts;
 
     // Constructor to initialize the ContactManager
-    public ContactManager() {
+    public ContactManager()
+    {
         contacts = new ArrayList<>();
     }
 
     // Method to add a new Contact
-    public void addContact(Contact contact) {
-        // Your code here
+    public void addContact(Contact contact)
+    {
+        contacts.add(contact);
     }
 
     // Method to remove a Contact by name
-    public void removeContact(String name) {
-        // Your code here
+    public void removeContact(String name)
+    {
+        Iterator<Contact> iterator = contacts.iterator();
+        while(iterator.hasNext())
+        {
+            Contact contact = iterator.next();
+            if(contact.getName().equals(name))
+            {
+                iterator.remove();;
+            }
+        }
     }
 
     // Method to list all Contacts
-    public void listContacts() {
-        // Your code here
+    public void listContacts()
+    {
+        for(Contact contact: contacts)
+        {
+            System.err.println(contact);
+        }
     }
-
-
 }
