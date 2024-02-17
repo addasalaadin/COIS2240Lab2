6e380d525ee1704a362428d706a9e81dc946df4a
Ay Pao Liu <aypaoliu@trentu.ca>
2024-02-16 22:51:16 -0500

    first

--- ContactManager.java
+++ ContactManager.java
@@ -15,16 +15,31 @@ class ContactManager {
     // Method to add a new Contact
     public void addContact(Contact contact) {
         // Your code here
+        contacts.add(contact);
+        System.out.println("Contact added: " + contact.getName());
     }
 
     // Method to remove a Contact by name
     public void removeContact(String name) {
         // Your code here
+        for (Contact contact : contacts) {
+            if (contact.getName().equals(name)) {
+                contacts.remove(contact);
+                System.out.println("Contact removed: " + name);
+                return; // Exit the loop once the contact is removed
+            }
+        }
+        System.out.println("Contact not found: " + name);
     }
 
     // Method to list all Contacts
     public void listContacts() {
         // Your code here
+        System.out.println("List of Contacts:");
+
+        for (Contact contact : contacts) {
+            System.out.println(contact.getName() + ": " + contact.getPhoneNumber() + " , " + contact.getEmail());
+        }
     }
 
 
