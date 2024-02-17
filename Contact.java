f4f65109b6dfd21cb02fea7345f756dd89fde802
Dikshith Reddy <dmacherla@trentu.ca>
2024-02-16 21:41:28 -0500

    Update 1

--- Contact.java
+++ Contact.java
@@ -1,31 +1,43 @@
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-// Define the Contact class
-class Contact {
-    // Properties of the Contact class
+public class Contact {
     private String name;
     private String email;
     private String phoneNumber;
 
-    // Constructor to initialize a new Contact object
+    // Constructor
     public Contact(String name, String email, String phoneNumber) {
         this.name = name;
         this.email = email;
         this.phoneNumber = phoneNumber;
     }
 
-    // Implement the toString method for Contact
-    @Override
-    public String toString() {
-        return "Contact{" +
-                "name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                '}';
+    // Getter and Setter methods
+    public String getName() {
+        return name;
     }
 
-    // Getters and Setters (if necessary)
-    // ...
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Contact{name='" + name + "', email='" + email + "', phoneNumber='" + phoneNumber + "'}";
+    }
 }
