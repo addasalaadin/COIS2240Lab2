516b80977c8e2e0aaa328bef57988642710cc0c5
MahirAhmed814 <mahirahmed@trentu.ca>
2024-02-16 23:48:26 -0500

    Completed lab solution

--- ContactManager.java
+++ ContactManager.java
@@ -14,18 +14,27 @@ class ContactManager {
 
     // Method to add a new Contact
     public void addContact(Contact contact) {
-        // Your code here
+        contacts.add(contact);
     }
 
     // Method to remove a Contact by name
     public void removeContact(String name) {
-        // Your code here
+        Iterator<Contact> iterator = contacts.iterator();
+        while (iterator.hasNext()) {
+            Contact contact = iterator.next();
+            if (contact.getName().equals(name)) {
+                iterator.remove();
+                System.out.println("Contact removed: " + contact);
+                return;
+            }
+        }
+        System.out.println("Contact not found with name: " + name);
     }
 
     // Method to list all Contacts
     public void listContacts() {
-        // Your code here
+        for (Contact contact : contacts) {
+            System.out.println(contact);
+        }
     }
-
-
 }
